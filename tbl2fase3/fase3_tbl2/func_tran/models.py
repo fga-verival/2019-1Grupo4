85fa25168062ce8d33ea61dceb9d0a7af0b545c9
mateusas3s <mateusaugusto-2009@hotmail.com>
2019-06-09 01:33:53 -0300

    criando model

--- tbl2fase3/fase3_tbl2/func_tran/models.py
+++ tbl2fase3/fase3_tbl2/func_tran/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Func_Tran(models.Model):
+    nome_func = models.CharField(max_length=64)
+    tipo_funcoes = models.CharField(max_length=2) ## EE, CE, SE
+    param1 = models.IntegerField()
+    param2 = models.IntegerField()
+    compl = models.CharField(max_length=6) ## baixa, media, alta -> preenchimento pela função
+    pont_func = models.IntegerField()
+    nome_cont = models.CharField(max_length=128)
+    data_cad = models.DateTimeField(default=timezone.now)
