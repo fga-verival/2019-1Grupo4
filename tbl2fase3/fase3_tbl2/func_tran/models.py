fd027054a394b3e9d8114753368b990ae7347be4
mateusas3s <mateusaugusto-2009@hotmail.com>
2019-06-09 01:50:29 -0300

    alterando campos da model

--- tbl2fase3/fase3_tbl2/func_tran/models.py
+++ tbl2fase3/fase3_tbl2/func_tran/models.py
@@ -2,11 +2,30 @@ from django.db import models
 from django.utils import timezone
 
 class Func_Tran(models.Model):
+    TIPO_FUNCOES = [
+        ('EE', 'Entradas Externas'),
+        ('CE', 'Consultas Externas'),
+        ('SE', 'Saídas Externas'),
+    ]
+
+    COMPLEXIDADE = [
+        ('B', 'Baixa'),
+        ('M', 'Média'),
+        ('A', 'Alta'),
+    ]
+
+    id = models.AutoField(primary_key=True)
     nome_func = models.CharField(max_length=64)
-    tipo_funcoes = models.CharField(max_length=2) ## EE, CE, SE
+    tipo_funcoes = models.CharField(
+        max_length=2,
+        choices=TIPO_FUNCOES,
+        default='EE') 
     param1 = models.IntegerField()
     param2 = models.IntegerField()
-    compl = models.CharField(max_length=6) ## baixa, media, alta -> preenchimento pela função
+    compl = models.CharField(
+        max_length=6,
+        choices=COMPLEXIDADE,
+        default='B')
     pont_func = models.IntegerField()
     nome_cont = models.CharField(max_length=128)
     data_cad = models.DateTimeField(default=timezone.now)
